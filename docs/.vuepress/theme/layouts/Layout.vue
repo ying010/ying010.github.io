25f17c6cfe30989f5c9f1cf2eadf00a807857e98
ying-tai <king.zhiying@gmail.com>
2023-02-17 17:02:44 +0800

    图片缩放

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -7,7 +7,6 @@
 import ParentLayout from '@vuepress/theme-default/layouts/Layout.vue'
 import {onMounted, watch} from 'vue'
 import {useRoute} from 'vue-router'
-import viewer from 'viewer'
 
 watch(
     () => useRoute(),
@@ -24,11 +23,27 @@ watch(
 
 
 onMounted(() => {
-  const el = document.getElementsByClassName('theme-default-content')?.[0]
-  if (el) {
-    console.log('mounted存在文章')
-    new Viewer(el)
-  } else console.log('不存在文章，不初始化')
+  let promise = new Promise(async function(resolve, reject) {
+    await function () {
+      const s = document.createElement('script');
+      s.src = 'https://cdn.bootcdn.net/ajax/libs/viewerjs/1.10.5/viewer.min.js';
+      document.head.appendChild(s);
+      console.info("加载viewer.js文件。。。")
+    }
+
+    setTimeout(() => resolve("done"), 1000);
+  });
+
+  promise.finally(() => {
+    console.info("加载viewer.js文件完成")
+    const el = document.getElementsByClassName('theme-default-content')?.[0]
+    if (el) {
+      console.log('mounted存在文章')
+      new Viewer(el)
+    } else console.log('不存在文章，不初始化')
+  });
+
+
 })
 </script>
 
